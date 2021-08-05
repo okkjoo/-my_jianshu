d8781c13fcf1c4f126c8c159b556c07d98bf7699
okkjoo <keyjo_zhou@m.scnu.edu.cn>
2021-08-05 11:40:31 +0800

    Header简单完成

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './common/header';
+import {GlobalStyled} from './style.js';
+
+function App() {
+  return (
+    <div>
+      <GlobalStyled />
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
