14388de1908709558b3d422a3c97fe159778d322
okkjoo <keyjo_zhou@m.scnu.edu.cn>
2021-08-06 18:52:07 +0800

    使用immutable保证纯函数

--- src/common/header/index.jsx
+++ src/common/header/index.jsx
@@ -56,7 +56,7 @@ const Header =(props)=>{
 
 const mapStateToProps = (state)=>{
 	return{
-		focused: state.header.focused
+		focused: state.header.get('focused')
 	}
 }
 const mapDispatchToProps = (dispatch) =>{
--- src/common/header/store/reducer.jsx
+++ src/common/header/store/reducer.jsx
@@ -1,20 +1,16 @@
 import * as constants from './constants'
-
-
-const defaultState = {
+import { fromJS } from 'immutable'
+const defaultState = fromJS({
   focused:false,
-}
+})
 const reducer = (state=defaultState,action)=>{
   const {type} = action
   switch (type){
     case constants.SEARCH_FOCUS:
-      return{
-        focused:true
-      } 
+      return state.set('focused',true)
     case constants.SEARCH_BLUR:
-      return{
-        focused:false
-      } 
+      return state.set('focused',false)
+      
 
     default:
       return state
