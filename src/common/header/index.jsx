6902cce374b8903b7874f397e8fb13ad72869c68
okkjoo <keyjo_zhou@m.scnu.edu.cn>
2021-08-06 19:08:51 +0800

    使用redux-immutable

--- src/common/header/index.jsx
+++ src/common/header/index.jsx
@@ -56,7 +56,7 @@ const Header =(props)=>{
 
 const mapStateToProps = (state)=>{
 	return{
-		focused: state.header.get('focused')
+		focused:state.getIn(['header','focused'])
 	}
 }
 const mapDispatchToProps = (dispatch) =>{
